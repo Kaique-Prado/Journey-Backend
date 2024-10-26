2a603975990651fbc1e05c7c095cd59623bb8c9c
Kaique-Prado <kaiqueprado59@gmail.com>
2024-10-26 20:27:19 -0300

    Envio dos arquivos do projeto

---
+++ src/main/java/com/journey_back/request/TripRequest.java
@@ -0,0 +1,6 @@
+package com.journey_back.request;
+
+import java.util.List;
+
+public record TripRequest(String destination, String starts_at, String ends_at, List<String> emails_to_invite, String owner_email, String owner_name) {
+}
