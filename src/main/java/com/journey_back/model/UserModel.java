e0a1819d627d95ca59ba0d33d3d93499f5054c1e
Kaique-Prado <kaiqueprado59@gmail.com>
2024-10-26 22:16:51 -0300

    Feat: Criação da tabela e entidade usuario

---
+++ src/main/java/com/journey_back/model/UserModel.java
@@ -0,0 +1,32 @@
+package com.journey_back.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany
+    @JoinColumn(name = "trip_id", nullable = false)
+    private TripModel tripId;
+
+}
